03cdf55deff58bbb6de724e103540e1ab383be03
samueljaybea <167593726+samueljaybea@users.noreply.github.com>
2024-05-05 16:32:09 +1000

    reformat code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,90 +1,103 @@
+function getComputerChoice() {
+  //randomly generates an int from 1-3
+  choice = Math.floor(Math.random() * 3) + 1;
 
-function getComputerChoice() { 
-    choice = Math.floor(Math.random() * 3) + 1; //randomly generates an int from 1-3
-
-    if (choice === 1) { //converts the int to a string and returns it's value
-        return "rock";
-    }
-    if (choice === 2) {
-        return "paper";
-    }
-    if (choice === 3) {
-        return "scissors";
-    }
+  //converts the int to a string and returns it's value
+  if (choice === 1) {
+    return "rock";
+  }
+  if (choice === 2) {
+    return "paper";
+  }
+  if (choice === 3) {
+    return "scissors";
+  }
 }
 
 function getPlayerChoice() {
-    choice = prompt("Rock, paper, or scissors?"); //prompts user for choice
-    choice = choice.toLowerCase(); //converts to lower case
-    if (choice == "rock" || choice == "paper" || choice == "scissors") { //checks for a valid choice
-        return choice; //returns the value
-    }
-    else {
-        return undefined; // hardcodes return of undefined on an invalid input
-    }
+  choice = prompt("Rock, paper, or scissors?"); //prompts user for choice
+  choice = choice.toLowerCase(); //converts to lower case
+
+  //checks for a valid choice
+  if (choice == "rock" || choice == "paper" || choice == "scissors") {
+    return choice; //returns the value
+  } else {
+    return undefined; // hardcodes return of undefined on an invalid input
+  }
 }
 
-function capitalize(word) { //capitalizes the first character in a string, reformats the inputs for the log
+//capitalizes the first character in a string, reformats the inputs for the log
+function capitalize(word) {
   word = word.charAt(0).toUpperCase() + word.slice(1);
   return word;
 }
 
 function playGame() {
-    let playerScore = 0; //initializes variable for player and computer scores
-    let computerScore = 0;
+  let playerScore = 0; //initializes variable for player and computer scores
+  let computerScore = 0;
 
-    let playerSelection; //initializes variable for player and computer selection
-    let computerSelection
+  let playerSelection; //initializes variable for player and computer selection
+  let computerSelection;
 
-    let roundNumber = 1; //initializes round number and total number of rounds
-    const totalRounds = 5;
+  let roundNumber = 1; //initializes round number and total number of rounds
+  const totalRounds = 5;
 
-    function playRound(playerChoice, computerChoice) {
-        if (playerChoice === computerChoice) //returns "tie" if computer and player choice are equal
-           return "Tie!";
+  function playRound(playerChoice, computerChoice) {
+    //returns "tie" if computer and player choice are equal
+    if (playerChoice === computerChoice) return "Tie!";
 
-        //outcomes if player wins
-        if (playerChoice == "rock" && computerChoice == "scissors" || playerChoice == "paper" && computerChoice == "rock" || playerChoice == "scissors" && computerChoice == "paper") { 
-            playerScore = ++playerScore;
-            return `You win! ${capitalize(playerChoice)} beats ${capitalize(computerChoice)}.`;
-        }
-        
-        //outcomes if computer wins
-        if (computerChoice == "rock" && playerChoice == "scissors" || computerChoice == "paper" && playerChoice == "rock" || computerChoice == "scissors" && playerChoice == "paper") {
-            computerScore = ++computerScore;
-            return `You lose :( ${capitalize(computerChoice)} beats ${capitalize(playerChoice)}.`
-        }
+    //outcomes if player wins
+    if (
+      (playerChoice == "rock" && computerChoice == "scissors") ||
+      (playerChoice == "paper" && computerChoice == "rock") ||
+      (playerChoice == "scissors" && computerChoice == "paper")
+    ) {
+      playerScore = ++playerScore;
+
+      return `You win! ${capitalize(playerChoice)} beats ${capitalize(computerChoice)}.`;
     }
 
-    for (roundNumber; roundNumber <= totalRounds;) { //replays the game for the specified amount of rounds
-        
-        playerSelection = getPlayerChoice(); //calls player choice function and assigns to variable
-        computerSelection = getComputerChoice();
+    //outcomes if computer wins
+    if (
+      (computerChoice == "rock" && playerChoice == "scissors") ||
+      (computerChoice == "paper" && playerChoice == "rock") ||
+      (computerChoice == "scissors" && playerChoice == "paper")
+    ) {
+      computerScore = ++computerScore;
+      return `You lose :( ${capitalize(computerChoice)} beats ${capitalize(playerChoice)}.`;
+    }
+  }
 
-        if (playerSelection == undefined) { //catches invalid inputs
-            console.log("Pick a valid option")
-            continue;
-        }
+  //replays the game for the specified amount of rounds
+  for (roundNumber; roundNumber <= totalRounds; ) {
+    playerSelection = getPlayerChoice(); //calls player choice function and assigns to variable
+    computerSelection = getComputerChoice();
 
-        console.log(`Round: ${roundNumber}`) //writes the round number into the log
-        console.log(playRound(playerSelection, computerSelection)); //writes the outcome to the log
-        console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`) // writes the scores to the log
-
-        ++roundNumber; //updates round number after a round
+    //catches invalid inputs
+    if (playerSelection == undefined) {
+      console.log("Pick a valid option");
+      continue;
     }
 
-    if (roundNumber > totalRounds) {  // writes outcome to the console
-        if (playerScore > computerScore) {
-            console.log ("The Player wins the game!")
-        }
-        if (playerScore < computerScore) {
-            console.log ("The Computer wins the game! :(")
-        }
-        if (playerScore === computerScore) {
-            console.log ("The game is a tie!")
-        }
-    }    
+    console.log(`Round: ${roundNumber}`); //writes the round number into the log
+    console.log(playRound(playerSelection, computerSelection)); //writes the outcome to the log
+    console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`); // writes the scores to the log
+
+    ++roundNumber; //updates round number after a round
+  }
+
+  // writes outcome to the console
+  if (roundNumber > totalRounds) {
+    if (playerScore > computerScore) {
+      console.log("The Player wins the game!");
+    }
+    if (playerScore < computerScore) {
+      console.log("The Computer wins the game! :(");
+    }
+    if (playerScore === computerScore) {
+      console.log("The game is a tie!");
+    }
+  }
 }
 
-playGame();
-
+playGame(); //calls game function
