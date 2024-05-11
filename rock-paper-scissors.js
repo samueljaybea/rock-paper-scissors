39daa7fb2b0756ef597fa9a022bf11fea4d83dfd
samueljaybea <167593726+samueljaybea@users.noreply.github.com>
2024-05-11 12:32:13 +1000

    added play again button. round outcome and game winning outcome now displayed in seperate divs and called correctly. all major js features are completed

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,14 +11,12 @@ const scoreTotal = 5; //set target score for a winner
 
 let roundCounter = document.querySelector('#round-counter');
 
-const playAgain = document.querySelector('#play-again');
-
 //identifies which button is clicked by it's ID and calls the playround function with the ID
 btns.forEach((btn) => {
 	btn.addEventListener('click', function () {
 		if (playerScore < scoreTotal && computerScore < scoreTotal) {
-			let choice = this.id;
-			results.textContent = playRound(choice, getComputerChoice());
+			let choice = this.id.toString();
+			playRound(choice, getComputerChoice());
 		}
 	});
 });
@@ -41,126 +39,59 @@ function playRound(playerChoice, computerChoice) {
 	roundNumber = ++roundNumber;
 	roundCounter.textContent = roundNumber;
 
-	if (playerChoice === computerChoice) return 'Tie!';
+	if (playerChoice === computerChoice) {
+		results.textContent = 'Tie!';
+	}
 
 	//outcomes if player wins
-	if (
+	else if (
 		(playerChoice == 'Rock' && computerChoice == 'Scissors') ||
 		(playerChoice == 'Paper' && computerChoice == 'Rock') ||
 		(playerChoice == 'Scissors' && computerChoice == 'Paper')
 	) {
 		playerScore = ++playerScore;
 		playerCounter.textContent = playerScore;
-		return playerScore === scoreTotal ? gameWin() : `You win! ${playerChoice} beats ${computerChoice}.`;
+		results.textContent = `You win! ${playerChoice} beats ${computerChoice}.`;
 	}
 
 	//outcomes if computer wins
 	else {
 		computerScore = ++computerScore;
 		computerCounter.textContent = computerScore;
-		return computerScore === scoreTotal ? gameWin() : `You lose :( ${computerChoice} beats ${playerChoice}.`;
+		results.textContent = `You lose :( ${computerChoice} beats ${playerChoice}.`;
+	}
+
+	if (computerScore === scoreTotal || playerScore === scoreTotal) {
+		gameWin();
 	}
 }
 
+//adds play again button
+const playAgain = document.querySelector('#play-again');
 const playButton = document.createElement('button');
+playButton.textContent = 'Play Again?';
 
-const gameOutcome = document.createElement('span');
+playButton.addEventListener('click', function () {
+	playerScore = 0;
+	computerScore = 0;
+	roundNumber = 1;
+	playerCounter.textContent = playerScore;
+	computerCounter.textContent = computerScore;
+	roundCounter.textContent = roundNumber;
+	results.textContent = '';
+	gameOutcome.textContent = '';
+	playButton.parentNode.removeChild(playButton);
+});
+
+const gameOutcome = document.createElement('div');
 
 function gameWin() {
+	playAgain.append(gameOutcome);
+	playAgain.append(playButton);
 	if (playerScore > computerScore) {
-		return 'The Player wins the game!';
+		gameOutcome.textContent = 'The Player wins the game!';
 	}
 	if (playerScore < computerScore) {
-		return 'The Computer wins the game! :(';
+		gameOutcome.textContent = 'The Computer wins the game! :(';
 	}
 }
-
-function roundWin() {}
-
-// function getWinner() {
-// 	if (playerScore > computerScore) {
-// 		console.log('x');
-// 		return 'The Player wins the game!';
-// 	}
-// 	if (playerScore < computerScore) {
-// 		console.log(y);
-// 		return 'The Computer wins the game! :(';
-// 	}
-// }
-//capitalizes the first character in a string, reformats the inputs for the log
-// function capitalize(word) {
-// 	word = word.charAt(0).toUpperCase() + word.slice(1);
-// 	return word;
-// }
-
-//determines the outcomes for a round
-
-// function playGame() {
-// 	let playerScore = 0; //initializes variable for player and computer scores
-// 	let computerScore = 0;
-
-// 	let playerSelection; //initializes variable for player and computer selection
-// 	let computerSelection;
-
-// 	//let roundNumber = 1; //initializes round number and total number of rounds
-// 	//const totalRounds = 5;
-
-// 	function playRound(playerChoice, computerChoice) {
-// 		//returns "tie" if computer and player choice are equal
-// 		if (playerChoice === computerChoice) return 'Tie!';
-
-// 		//outcomes if player wins
-// 		if (
-// 			(playerChoice == 'rock' && computerChoice == 'scissors') ||
-// 			(playerChoice == 'paper' && computerChoice == 'rock') ||
-// 			(playerChoice == 'scissors' && computerChoice == 'paper')
-// 		) {
-// 			playerScore = ++playerScore;
-
-// 			return `You win! ${capitalize(playerChoice)} beats ${capitalize(computerChoice)}.`;
-// 		}
-
-// 		//outcomes if computer wins
-// 		if (
-// 			(computerChoice == 'rock' && playerChoice == 'scissors') ||
-// 			(computerChoice == 'paper' && playerChoice == 'rock') ||
-// 			(computerChoice == 'scissors' && playerChoice == 'paper')
-// 		) {
-// 			computerScore = ++computerScore;
-// 			return `You lose :( ${capitalize(computerChoice)} beats ${capitalize(playerChoice)}.`;
-// 		}
-//	}
-
-//replays the game for the specified amount of rounds
-// for (roundNumber; roundNumber <= totalRounds; ) {
-// 	playerSelection = getPlayerChoice(); //calls player choice function and assigns to variable
-// 	computerSelection = getComputerChoice();
-
-// 	//catches invalid inputs
-// 	if (playerSelection == undefined) {
-// 		console.log('Pick a valid option');
-// 		continue;
-// 	}
-
-// 	console.log(`Round: ${roundNumber}`); //writes the round number into the log
-// 	console.log(playRound(playerSelection, computerSelection)); //writes the outcome to the log
-// 	console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`); // writes the scores to the log
-
-// 	++roundNumber; //updates round number after a round
-// }
-
-// writes game outcome to the console
-// 	if (roundNumber > totalRounds) {
-// 		if (playerScore > computerScore) {
-// 			console.log('The Player wins the game!');
-// 		}
-// 		if (playerScore < computerScore) {
-// 			console.log('The Computer wins the game! :(');
-// 		}
-// 		if (playerScore === computerScore) {
-// 			console.log('The game is a tie!');
-// 		}
-// 	}
-// }
-
-// playGame(); //calls game function
